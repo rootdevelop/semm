bdb90e3bb34c93f6567aadf2dd83938c70e49fae
duco.fronik <duco.fronik@capgemini.com>
2016-04-24 01:19:47 +0200

    WindDirection

---
+++ KeepOnDroning.Api/src/KeepOnDroning.Api/Helpers/WindDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeepOnDroning.Api.Helpers
+{
+    public static class WindDirection
+    {
+        public static string WindDegreesToDirection(float deg)
+        {
+            var val =((deg/22.5) + .5);
+            var index = (int)Math.Round(val%16);           
+            return DroningConstants.WindDirections[index];
+        }
+    }
+}
