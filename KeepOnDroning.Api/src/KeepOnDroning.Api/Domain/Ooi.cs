798e3d046c319662d0fadc06b033221d7fe35df4
Xciles <xciles@gmail.com>
2016-04-24 00:35:55 +0200

    Adding Ooi!

---
+++ KeepOnDroning.Api/src/KeepOnDroning.Api/Domain/Ooi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using KeepOnDroning.Api.Domain.Enums;
+
+namespace KeepOnDroning.Api.Domain
+{
+    [Table("Oois")]
+    public class Ooi
+    {
+        [Key]
+        public long Id { get; set; }
+        public string UniqueIdentifier { get; set; }
+        public string Name { get; set; }
+        public double CurrentLat { get; set; }
+        public double CurrentLng { get; set; }
+        public double DestinationLat { get; set; }
+        public double DestinationLng { get; set; }
+        public double Speed { get; set; }
+        public EOoiType Type { get; set; }
+    }
+}
