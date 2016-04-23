f6746686fbf8a60ec0dfdd06ffbcf032a81fee02
Appiekap <Appiekap@Ricks-Mac-mini.local>
2016-04-24 00:28:11 +0200

    Added iOS game view

---
+++ App/KeepOnDroning/KeepOnDroning.iOS/Views/GameView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using UIKit;
+using MvvmCross.iOS.Views;
+
+namespace KeepOnDroning.iOS
+{
+	public partial class GameView : MvxViewController
+	{
+		public GameView () : base ("GameView", null)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			if (GameUIView != null)
+			{
+				// Set loading event to be called once game view is fully initialised
+				//GameUIView.ViewCreated += GameDelegate.LoadGame;
+			}
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+
+			if (GameUIView != null)
+				GameUIView.Paused = true;
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+
+			if (GameUIView != null)
+				GameUIView.Paused = false;
+		}
+	}
+}
+
+
