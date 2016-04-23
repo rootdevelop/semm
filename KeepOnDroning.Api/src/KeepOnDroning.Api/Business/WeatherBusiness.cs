1a2be6172215fc74fe20a08dbf2696ca6eb497cd
Xciles <xciles@gmail.com>
2016-04-23 20:02:04 +0200

    Changes to busienss

--- KeepOnDroning.Api/src/KeepOnDroning.Api/Business/WeatherBusiness.cs
+++ KeepOnDroning.Api/src/KeepOnDroning.Api/Business/WeatherBusiness.cs
@@ -4,22 +4,28 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using KeepOnDroning.Api.ServiceDomain;
+using Newtonsoft.Json;
 
 namespace KeepOnDroning.Api.Business
 {
     public class WeatherBusiness
     {
-        private string _openWeatherMapUri = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}";
+        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects };
+
+        private readonly string _openWeatherMapUri = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}";
         private string _wheaterAppId = "37bafeb001ed3617ec71a179eaf594ce"; 
+
         public async Task<Weather> GetWeather(float latitude, float longitude)
         {
-            //var client = new UncommonHttpClient();
-            //var uri = String.Format(_openWeatherMapUri, latitude, longitude, _wheaterAppId);
+            using (var client = new HttpClient())
+            {
+                var uri = String.Format(_openWeatherMapUri, latitude, longitude, _wheaterAppId);
+                var httpResponseMessage = await client.GetAsync(uri);
+                httpResponseMessage.EnsureSuccessStatusCode();
 
-            //var weather = await client.GetJsonAsync<Weather>(uri, HttpCompletionOption.ResponseContentRead);
-
-            //return weather;
-            return null;
+                var resultAsString = await httpResponseMessage.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Weather>(resultAsString, _jsonSerializerSettings);
+            }
         }
     }
 }
