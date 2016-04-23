fc04ea32c4faccd7bcd2caed1da87e25faf80590
Appiekap <Appiekap@Ricks-Mac-mini.local>
2016-04-23 23:33:40 +0200

    En deze ook

---
+++ Game/CrashDrone/CrashDrone/CrashDrone.iOS/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using UIKit;
+using CrashDrone.Common;
+
+namespace CrashDrone.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController () : base ("ViewController", null)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			if (GameView != null)
+			{
+				// Set loading event to be called once game view is fully initialised
+				GameView.ViewCreated += GameDelegate.LoadGame;
+			}
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+
+			if (GameView != null)
+				GameView.Paused = true;
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+
+			if (GameView != null)
+				GameView.Paused = false;
+		}
+	}
+}
+
+
