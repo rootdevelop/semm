6e1be53e417ac934111faae8c1716a0a8b8d508e
Jeffrey Klink Logeman <klingu@gmail.com>
2016-04-23 20:45:38 +0200

    Added visibility plugin

---
+++ App/KeepOnDroning/KeepOnDroning.iOS/Bootstrap/VisibilityPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace KeepOnDroning.iOS.Bootstrap
+{
+    public class VisibilityPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader, MvvmCross.Plugins.Visibility.iOS.Plugin>
+    {
+    }
+}
