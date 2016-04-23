980c23389249e3b5998d655eeea102d293738a1f
Xciles <xciles@gmail.com>
2016-04-23 19:56:11 +0200

    Adding ICAO

--- KeepOnDroning.Api/src/KeepOnDroning.Api/Domain/Airport.cs
+++ KeepOnDroning.Api/src/KeepOnDroning.Api/Domain/Airport.cs
@@ -14,6 +14,7 @@ namespace KeepOnDroning.Api.Domain
         public string City { get; set; }
         public string Country { get; set; }
         public string Iata { get; set; }
+        public string Icao { get; set; }
         public double Lat { get; set; }
         public double Lng { get; set; }
         public int Altitude { get; set; }
