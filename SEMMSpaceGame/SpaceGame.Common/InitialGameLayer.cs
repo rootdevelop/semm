de0e2c2dd21a847e8de094c405c75c0974bf227a
ROEJANSE <ROEJANSE@LBNL12272>
2016-04-23 14:59:40 +0200

    Background halved on height and width

--- SEMMSpaceGame/SpaceGame.Common/InitialGameLayer.cs
+++ SEMMSpaceGame/SpaceGame.Common/InitialGameLayer.cs
@@ -22,7 +22,7 @@ namespace SpaceGame.Common
 		{
 			base.AddedToScene ();
 
-			tileMap = new CCTileMap ("tilemaps/level0.tmx");
+			tileMap = new CCTileMap ("tilemaps/Background_Level_0.tmx");
 			this.AddChild (tileMap);
 
 			helloLabel = new CCLabel("Hello Space Apps!", "Arial", 30, CCLabelFormat.SystemFont);
