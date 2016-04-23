21dbdfc7256ddd0ee607a251624bcf183bd55507
Jeffrey Klink Logeman <klingu@gmail.com>
2016-04-23 20:05:07 +0200

    Added images added buttons added binding from buttons to commands

--- App/KeepOnDroning/KeepOnDroning.iOS/Views/PreFlightCheckView.designer.cs
+++ App/KeepOnDroning/KeepOnDroning.iOS/Views/PreFlightCheckView.designer.cs
@@ -1,22 +1,58 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio Business to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace KeepOnDroning.iOS
 {
 	[Register ("PreFlightCheckView")]
 	partial class PreFlightCheckView
 	{
+		[Outlet]
+		UIKit.UIButton btnCustom { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnGame { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnInfo { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnMap { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnStart { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnStart != null) {
+				btnStart.Dispose ();
+				btnStart = null;
+			}
+
+			if (btnMap != null) {
+				btnMap.Dispose ();
+				btnMap = null;
+			}
+
+			if (btnInfo != null) {
+				btnInfo.Dispose ();
+				btnInfo = null;
+			}
+
+			if (btnGame != null) {
+				btnGame.Dispose ();
+				btnGame = null;
+			}
+
+			if (btnCustom != null) {
+				btnCustom.Dispose ();
+				btnCustom = null;
+			}
 		}
 	}
 }
-
