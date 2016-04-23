6e284a6fdbc304c7f2745539c0a207893c792e41
Xciles <xciles@gmail.com>
2016-04-24 01:21:27 +0200

    Adding Ooi things

---
+++ KeepOnDroning.Api/src/KeepOnDroning.Api/Controllers/EternityController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KeepOnDroning.Api.Business;
+using KeepOnDroning.Api.ServiceDomain;
+using Microsoft.AspNet.Mvc;
+
+namespace KeepOnDroning.Api.Controllers
+{
+    [Route("api/Eternity")]
+    public class EternityController : Controller
+    {
+        private EternityBusiness _eternityBusiness;
+
+        public EternityController(EternityBusiness eternityBusiness)
+        {
+            _eternityBusiness = eternityBusiness;
+        }
+
+        [Route("Azshara")]
+        [HttpPost]
+        public async Task Azshara(OoiRequest legion)
+        {
+            await _eternityBusiness.Summon(legion);
+        }
+    }
+}
