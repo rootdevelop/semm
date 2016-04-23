731a76d0d83ea44a487d2827671e472b8ef31ac5
Xciles <xciles@gmail.com>
2016-04-24 00:37:33 +0200

    Adding ooi migration!

--- KeepOnDroning.Api/src/KeepOnDroning.Api/Migrations/20160423223704_AddOoi.Designer.cs
+++ KeepOnDroning.Api/src/KeepOnDroning.Api/Migrations/20160423223704_AddOoi.Designer.cs
@@ -8,9 +8,10 @@ using KeepOnDroning.Api.Data;
 namespace KeepOnDroning.Api.Migrations
 {
     [DbContext(typeof(DroningDbContext))]
-    partial class DroningDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160423223704_AddOoi")]
+    partial class AddOoi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
