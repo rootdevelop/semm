cd6750dbd4703aa471600166870bc914268ccb3c
elblondo <appic.ideas@gmail.com>
2016-04-23 19:18:55 +0200

    cleaned peripherylayer; added images

--- Game/CrashDrone/CrashDrone/CrashDrone/Layers/PeripheryLayer.cs
+++ Game/CrashDrone/CrashDrone/CrashDrone/Layers/PeripheryLayer.cs
@@ -8,17 +8,8 @@ namespace CrashDrone.Common
     public class PeripheryLayer : CCLayerColor
     {
 
-        // Define a label variable
-        CCLabel label;
-
         public PeripheryLayer() : base(CCColor4B.Transparent)
         {
-
-            // create and initialize a Label
-            label = new CCLabel("Periphery", "Fonts/MarkerFelt", 22, CCLabelFormat.SpriteFont);
-
-            // add the label as a child to this Layer
-            AddChild(label);
 
         }
 
@@ -26,24 +17,7 @@ namespace CrashDrone.Common
         {
             base.AddedToScene();
 
-            // Use the bounds to layout the positioning of our drawable assets
-            var bounds = VisibleBoundsWorldspace;
-
-            // position the label on the center of the screen
-            label.Position = bounds.Center;
-
-            // Register for touch events
-            var touchListener = new CCEventListenerTouchAllAtOnce();
-            touchListener.OnTouchesEnded = OnTouchesEnded;
-            AddEventListener(touchListener, this);
-        }
-
-        void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
-        {
-            if (touches.Count > 0)
-            {
-                // Perform touch handling here
-            }
+            
         }
 	}
 }
