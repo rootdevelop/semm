e53d9f93163568dd792065e1e801b34aa57745a3
RootDevelop <robbert@rootdevelop.nl>
2016-04-24 02:58:42 +0200

    Whoops

--- App/KeepOnDroning/KeepOnDroning.Droid/Bootstrap/UserDialogsPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace KeepOnDroning.Droid.Bootstrap
-{
-    public class UserDialogsPluginBootstrap
-        : MvxPluginBootstrapAction<Acr.MvvmCross.Plugins.UserDialogs.PluginLoader>
-    {
-    }
-}
