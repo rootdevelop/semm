564db59302475729221fc112c81dc2f1499a4f29
Appiekap <Appiekap@Ricks-Mac-mini.local>
2016-04-24 04:11:49 +0200

    fok die downloadcache

--- App/KeepOnDroning/KeepOnDroning.Droid/Bootstrap/DownloadCachePluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace KeepOnDroning.Droid.Bootstrap
-{
-    public class DownloadCachePluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.DownloadCache.PluginLoader>
-    {
-    }
-}
