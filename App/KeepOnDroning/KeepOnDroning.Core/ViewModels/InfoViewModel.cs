2c4890ce0a74cd5d09febbac48c3b14ae9d6d550
Jeffrey Klink Logeman <klingu@gmail.com>
2016-04-24 09:12:46 +0200

    Added InfoViewModel

---
+++ App/KeepOnDroning/KeepOnDroning.Core/ViewModels/InfoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MvvmCross.Core.ViewModels;
+using System.Windows.Input;
+
+namespace KeepOnDroning.Core
+{
+	public class InfoViewModel : MvxViewModel
+	{
+		public InfoViewModel ()
+		{
+		}
+
+		public ICommand GoBackCommand
+		{
+			get
+			{
+				return new MvxCommand(() =>
+					{
+						Close(this);
+					});
+			}
+		}
+	}
+}
