4a4cccad1b0f14fedd470d019c450dfb1eb16181
Appiekap <Appiekap@Ricks-Mac-mini.local>
2016-04-24 02:08:32 +0200

    update

--- Game/CrashDrone/CrashDrone/CrashDrone/GameDelegate.cs
+++ Game/CrashDrone/CrashDrone/CrashDrone/GameDelegate.cs
@@ -27,7 +27,7 @@ namespace CrashDrone.Common
                 // Determine whether to use the high or low def versions of our images
                 // Make sure the default texel to content size ratio is set correctly
                 // Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
-				/*
+
                 if (width < viewSize.Width)
                 {
                     //contentSearchPaths.Add("Images/Collision");
@@ -38,7 +38,7 @@ namespace CrashDrone.Common
                     //contentSearchPaths.Add("Images/Periphery");
                     CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
                 }
-                */
+                
 
                 gameView.ContentManager.SearchPaths = contentSearchPaths;
 
