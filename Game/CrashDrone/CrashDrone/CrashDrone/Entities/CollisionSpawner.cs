0d5dd2afd92f669faf1ab444b3679754358a8543
elblondo <appic.ideas@gmail.com>
2016-04-24 11:21:47 +0200

    battery

--- Game/CrashDrone/CrashDrone/CrashDrone/Entities/CollisionSpawner.cs
+++ Game/CrashDrone/CrashDrone/CrashDrone/Entities/CollisionSpawner.cs
@@ -20,6 +20,13 @@ namespace CrashDrone.Common.Entities
             set;
         }
 
+        private float _timeSinceLastBatterySpawn;
+        public float TimeInbetweenBatterySpawns
+        {
+            get;
+            set;
+        }
+
         private float _timeSinceLastGroundUnitSpawn;
         public float TimeInbetweenGroundUnitSpawns
         {
@@ -38,16 +45,22 @@ namespace CrashDrone.Common.Entities
 
             _timeSinceLastGroundUnitSpawn = 0.0f;
             TimeInbetweenGroundUnitSpawns = 8f;
+
+            _timeSinceLastBatterySpawn = 0.0f;
+            TimeInbetweenBatterySpawns = 15f;
+
         }
 
         public void Activity(float frameTime)
         {
             _timeSinceLastBirdSpawn += frameTime;
             _timeSinceLastGroundUnitSpawn += frameTime;
+            _timeSinceLastBatterySpawn += frameTime;
 
 
             if (_timeSinceLastBirdSpawn > TimeInbetweenBirdSpawns)
             {
+                _timeSinceLastBirdSpawn = 0.0f;
                 SpawnBird();
             }
 
@@ -56,13 +69,32 @@ namespace CrashDrone.Common.Entities
                 _timeSinceLastGroundUnitSpawn = 0.0f;
                 SpawnGroundUnit();
             }
-            
+
+            if (_timeSinceLastBatterySpawn > TimeInbetweenBatterySpawns)
+            {
+                _timeSinceLastBatterySpawn = 0.0f;
+                SpawnBattery();
+            }
+
+        }
+
+        private void SpawnBattery()
+        {
+            TimeInbetweenBatterySpawns = CCRandom.GetRandomFloat(15f, 45f);
+
+            var collisionEntity = new Battery();
+            collisionEntity.PositionX = _layer.VisibleBoundsWorldspace.MaxX + collisionEntity.GraphicWidth * 0.5f;
+            collisionEntity.PositionY = CCRandom.GetRandomFloat(_layer.VisibleBoundsWorldspace.MaxY * 0.2f, _layer.VisibleBoundsWorldspace.MaxY * 0.7f);
+
+            if (EntitySpawned != null)
+            {
+                EntitySpawned(collisionEntity);
+            }
         }
 
         private void SpawnBird()
         {
             TimeInbetweenBirdSpawns = CCRandom.GetRandomFloat(1.6f, 5f);
-            _timeSinceLastBirdSpawn = 0.0f;
 
             var collisionEntity = new Bird();
             collisionEntity.PositionX = _layer.VisibleBoundsWorldspace.MaxX + collisionEntity.GraphicWidth * 0.5f;
@@ -72,7 +104,6 @@ namespace CrashDrone.Common.Entities
             {
                 EntitySpawned(collisionEntity);
             }
-            _isSpawning = false;
         }
 
         private void SpawnGroundUnit()
